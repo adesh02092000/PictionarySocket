da21a26670239c15003089c13d9976f22cbb24b2
Adesh Gupta <adesh02092000@gmail.com>
2022-02-26 18:57:55 +0530

    accessed room Id and user name

--- client/room.js
+++ client/room.js
@@ -3,5 +3,10 @@ import { io } from "socket.io-client"
 const production = process.env.NODE_ENV === "production"
 const serverUrl = production ? "realsite.com" : "http://localhost:3000"
 
+const urlParams = new URLSearchParams(window.location.search)
+const name = urlParams.get("name")
+const roomId = urlParams.get("room-id")
+console.log(name, roomId)
+
 const socket = io(serverUrl)
 console.log(socket)
