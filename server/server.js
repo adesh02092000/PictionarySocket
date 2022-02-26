1f2f0b837b7313cb6c8ba50d74ddd0034264526c
Adesh Gupta <adesh02092000@gmail.com>
2022-02-26 14:51:10 +0530

    Added Client URL as option to server, to abide the CORS policy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const { Server } = require("socket.io")
 
-const io = new Server({})
+const io = new Server({
+  cors: {
+    origin: "http://localhost:1234",
+  },
+})
 io.on("connection", socket => {
   console.log("Connected Successfully")
 })
